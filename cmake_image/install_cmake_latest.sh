9f3451bd23ff52941ce24bb3389d09ca0bb165e5
Alex Schwartz <schwartz_alex@pm.me>
2023-03-30 00:38:40 -0400

    * Update CMake version

--- cmake_image/install_cmake_latest.sh
+++ cmake_image/install_cmake_latest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-CMAKE_VERSION=3.25.2
+CMAKE_VERSION=3.26.1
 
 wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION.tar.gz
 tar -xzvf cmake-$CMAKE_VERSION.tar.gz
 cd cmake-$CMAKE_VERSION
 ./bootstrap --prefix=/opt/cmake
-make 
+make -j$(nproc)
 make install
