5e2d8a86fbf05c4cbdff8969bb26aa6e9249e668
Alex Schwartz <schwartz_alex@pm.me>
2024-01-27 12:30:48 -0500

    * Added selene image folder, added a buildah shell script (added documentation for how to enable)

---
+++ assemble_images_builah.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# boost
+buildah build -f ./boost_image/Dockerfile.boost -t boost:latest ./boost_image
+
+# cmake
+buildah build -f ./cmake_image/Dockerfile.cmake -t cmake:latest ./cmake_image
+
+# grpc
+buildah build -f ./grpc_image/Dockerfile.grpc -t grpc:latest ./grpc_image
+
+# Intel MKL
+buildah build -f ./intel_mkl_image/Dockerfile.mkl -t intel-mkl:latest ./intel_mkl_image
+
+# Intel TBB
+buildah build -f ./intel_tbb_image/Dockerfile.tbb -t intel-tbb:latest ./intel_tbb_image
+
+# Selene
+buildah build -f ./selene_image/Dockerfile.selene -t selene:latest ./selene_image
+
+# meson-build (final build image)
+buildah build -f ./Dockerfile.meson-build -t meson-build:latest .
