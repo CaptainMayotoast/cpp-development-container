bc556383cf20ed92fe6c5b843ffe0f24e6ee63ee
Alex Schwartz <schwartz_alex@pm.me>
2024-02-22 00:06:15 -0500

    * Spelling mistake corrected in script file

---
+++ gcc12/assemble_images_buildah.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# boost
+buildah build -f ./boost_image/Dockerfile.boost -t boost:12.2 ./boost_image
+
+# cmake
+buildah build -f ./cmake_image/Dockerfile.cmake -t cmake:12.2 ./cmake_image
+
+# Intel MKL
+buildah build -f ./intel_mkl_image/Dockerfile.mkl -t intel-mkl:12.2 ./intel_mkl_image
+
+# Intel TBB
+buildah build -f ./intel_tbb_image/Dockerfile.tbb -t intel-tbb:12.2 ./intel_tbb_image
+
+# meson-build (final build image)
+buildah build -f ./Dockerfile.meson-build -t meson-build:12.2 .
