59ffb0e70caf1d70256736638918f129302247cd
Alex Schwartz <schwartz_alex@pm.me>
2024-12-25 16:22:30 -0500

    * Updated to GCC 14.2, while also porting in GCC 13.3 for NVCC (Python CUDA/C++ module image_to_ascii can be invoked)

--- gcc13/assemble_images_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# boost
-docker build -f ./boost_image/Dockerfile.boost -t boost:13.3 ./boost_image
-
-# meson-build (final build image)
-docker build -f ./Dockerfile.meson-build -t meson-build:13.3 .
